8b4b2d112422aa38b269761985b3e969ca6ff2d1
Ayaabudagar <ayakhlil23@yahoo.com>
2021-04-01 14:13:14 +0300

    edit js app

---
+++ app.js
@@ -0,0 +1,14 @@
+alert("welcom to my bage");
+var name = prompt("please enter your name", "your name");
+console.log(name);
+alert("thank you for visiting our page " + name);
+var dsg = prompt("are you a student " + name + "??", "yes or no");
+console.log(dsg);
+if (dsg === "yes")
+{
+    alert("welcome abored to this usful page !");
+}
+else
+{
+    alert("you are missing alot of study");
+}
